12db0d15320fa51bfc9e01af1903b89e3b6c3044
Ricardo Rebelo Jr <ricardorebelojr@gmail.com>
2023-04-27 20:55:58 -0300

    :recycle: (update) Ajuste do código

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./pages/header/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <p>Header</p>
-      </header>
+      <Header />
     </div>
   );
 }
