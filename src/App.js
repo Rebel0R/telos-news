2238750547d8ed560ba5199e20c55e19f66623a2
Ricardo Rebelo Jr <ricardorebelojr@gmail.com>
2023-04-27 23:25:44 -0300

    :sparkles: (style) Criação do componente de estilização do App.js para testes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import Card from "./components/card/Card";
 import Header from "./components/header/Header";
+import MiniCard from "./components/mini-card/MiniCard";
+import "./AppStyle.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="content-cards1">
+        <Card />
+        <div>
+          <MiniCard />
+          <MiniCard />
+        </div>
+      </div>
     </div>
   );
 }
