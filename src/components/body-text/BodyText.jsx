763e2ca3f6759ae697afe9f482e1921e0debabf2
Ricardo Rebelo Jr <ricardorebelojr@gmail.com>
2023-04-28 22:39:40 -0300

    :sparkles: (feat) Criação do componente Body Text para o corpo da notícia

---
+++ src/components/body-text/BodyText.jsx
@@ -0,0 +1,21 @@
+import "./BodyText.css";
+import React from "react";
+
+export default function BodyText() {
+  return (
+    <div className="body-text">
+      <p className="news-resume">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas amet nulla quis ab! Quae,
+        reiciendis.
+      </p>
+      <p className="news-title">Bournout existe ou não?</p>
+      <p className="news-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima tempore earum, quaerat
+        reprehenderit explicabo quasi incidunt saepe ut veritatis dignissimos dolorem sit similique
+        est nam animi quidem aut id expedita voluptatem cupiditate eaque repellendus atque tempora!
+        Eius provident minima architecto nemo, vero magni ullam dolor consectetur eaque vitae
+        blanditiis unde?
+      </p>
+    </div>
+  );
+}
