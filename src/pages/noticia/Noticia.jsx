b8aa883283db310a6b1904bacbc616d69b5628c1
Ricardo Rebelo Jr <ricardorebelojr@gmail.com>
2023-04-28 22:12:37 -0300

    :construction: (att) Atualização de arquivos

--- src/pages/noticia/Noticia.jsx
+++ src/pages/noticia/Noticia.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "../../components/header/Header";
+import Banner from "../../components/banner/Banner";
 
 export default function Noticia() {
   return (
-    <div>
+    <div className="noticia">
       <Header />
-      <h1>Noticia</h1>
+      <div className="content-noticia">
+        <Banner />
+      </div>
     </div>
   );
 }
