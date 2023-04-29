65074198d5fa776d16fc4f0bb833f365c4c432be
Ricardo Rebelo Jr <ricardorebelojr@gmail.com>
2023-04-29 00:10:58 -0300

    :sparkles: (att) Atualização do código

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Noticia from "./pages/news/News.jsx";
+import News from "./pages/news/News.jsx";
 import Home from "./pages/home/Home.jsx";
 
 export default function AppRoutes() {
@@ -8,7 +8,7 @@ export default function AppRoutes() {
     <BrowserRouter>
       <Routes>
         <Route element={<Home />} path="/" exact />
-        <Route element={<Noticia />} path="/noticia" />
+        <Route element={<News />} path="/noticia" />
       </Routes>
     </BrowserRouter>
   );
