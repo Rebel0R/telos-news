f1a33099f38ae153b8b8897730eaa38dbe15108c
Ricardo Rebelo Jr <ricardorebelojr@gmail.com>
2023-04-29 01:06:05 -0300

    :sparkles: (route) Gerenciamento dos dados das notícias, e também das rotas

--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import Header from "../../components/header/Header";
 import Banner from "../../components/banner/Banner";
 import BodyText from "../../components/body-text/BodyText";
+import news from "../../data/news-data";
+import { useParams } from "react-router-dom";
 
 export default function News() {
+  const { id } = useParams();
+  const currentNews = news.find((result) => result.id === id);
+  console.log("News:" + news);
+  console.log("ID:" + id);
+
   return (
     <div className="noticia">
       <Header />
       <div className="content-noticia">
-        <Banner />
-        <BodyText />
+        <Banner news={currentNews} />
+        <BodyText news={currentNews} />
       </div>
     </div>
   );
