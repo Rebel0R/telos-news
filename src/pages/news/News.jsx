29e0ccf66edbd4340cf3fda53cbbca17e2878c4b
Ricardo Rebelo Jr <ricardorebelojr@gmail.com>
2023-04-29 00:10:19 -0300

    :sparkles: (att) Atualizaçôes e correções do código

--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -3,14 +3,14 @@ import Header from "../../components/header/Header";
 import Banner from "../../components/banner/Banner";
 import BodyText from "../../components/body-text/BodyText";
 
-export default function Noticia() {
+export default function News() {
   return (
-    <body className="noticia">
+    <div className="noticia">
       <Header />
       <div className="content-noticia">
         <Banner />
         <BodyText />
       </div>
-    </body>
+    </div>
   );
 }
